cd591122633e531a6c7423124b75459bfe00d50f
Fernando Alves <fx2.sistemas@gmail.com>
2020-11-08 12:37:38 -0300

    dev-nando add articles to tags

--- src/Controller/ArticlesController.php
+++ src/Controller/ArticlesController.php
@@ -38,15 +38,20 @@ class ArticlesController extends AppController
             }
             $this->Flash->error(__('Unable to add your article.'));
         }
+        $tags = $this->Articles->Tags->find('list')->all();
+
+        $this->set('tags', $tags);
+
         $this->set('article', $article);
     }
+
 
     public function edit($slug)
     {
         $article = $this->Articles
             ->findBySlug($slug)
+            ->contain('Tags') // load associated Tags
             ->firstOrFail();
-
         if ($this->request->is(['post', 'put'])) {
             $this->Articles->patchEntity($article, $this->request->getData());
             if ($this->Articles->save($article)) {
@@ -55,6 +60,12 @@ class ArticlesController extends AppController
             }
             $this->Flash->error(__('Unable to update your article.'));
         }
+
+        // Get a list of tags.
+        $tags = $this->Articles->Tags->find('list')->all();
+
+        // Set tags to the view context
+        $this->set('tags', $tags);
 
         $this->set('article', $article);
     }
